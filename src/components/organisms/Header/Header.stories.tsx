63416605b601a67675213f125bcc5d0114ddf44d
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-17 14:13:20 +0900

    #5 storybookを追加

---
+++ src/components/organisms/Header/Header.stories.tsx
@@ -0,0 +1,9 @@
+import Header from '.';
+
+export default {
+  title: 'component/organisms/Header',
+  component: Header,
+};
+const Template = () => <Header></Header>;
+
+export const Default = Template.bind({});
