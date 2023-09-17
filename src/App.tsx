f63c1c9e2dd43078cf7fa1a8a1280d4c1a70e927
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-17 15:04:43 +0900

    #8 ファイル名を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Container } from '@mui/material';
 import AddTaskForm from './components/organisms/AddTaskForm/AddTaskForm';
-import Header from './components/organisms/Header';
-import TaskList from './components/organisms/TaskList';
+import Header from './components/organisms/Header/Header';
+import TaskList from './components/organisms/TaskList/TaskList';
 
 const App = (): JSX.Element => {
   const [tasks, setTasks] = useState<string[]>([]);
