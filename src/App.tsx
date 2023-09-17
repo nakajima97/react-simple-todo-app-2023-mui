0cc3d1214f5b6514c14899f75ab71e77d19db63b
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-17 11:52:01 +0900

    #1 大まかな設計を反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { AppBar, Box, Container, Typography, Toolbar } from '@mui/material';
 
 const App = (): JSX.Element => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Todo App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box>タスク追加欄</Box>
+      <Box>タスク一覧</Box>
+    </Container>
   );
 };
 
