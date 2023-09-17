0f6a95f2c06891f579b36ba4c2f4e2a283eac727
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-17 14:36:26 +0900

    #5 最新バージョンの書き方に合わせた

---
+++ src/components/organisms/TaskList/TaskList.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TaskList from '.';
+
+const meta = {
+  title: 'component/organisms/TaskList',
+  component: TaskList,
+} satisfies Meta<typeof TaskList>;
+export default meta;
+
+type Story = StoryObj<typeof TaskList>;
+
+export const Default: Story = {
+  args: {
+    tasks: ['タスク1', 'タスク2', 'タスク3'],
+  },
+};
