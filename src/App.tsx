7a79e5b9eb465399b40f73d9260f1f3918c217d5
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-17 13:30:47 +0900

    #5 タスクリストをorganismsに分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,17 @@
 import { useState } from 'react';
-import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  Typography,
-  Toolbar,
-  TextField,
-} from '@mui/material';
+import { Container } from '@mui/material';
 import AddTaskForm from './components/organisms/AddTaskForm';
 import Header from './components/organisms/Header';
+import TaskList from './components/organisms/TaskList';
 
 const App = (): JSX.Element => {
   const [tasks, setTasks] = useState<string[]>(['aaa', 'bbb', 'ccc']);
-
-  const renderTodoList = () => {
-    if (tasks.length === 0) {
-      return <Typography>タスクが0件です。</Typography>;
-    }
-
-    return tasks.map((task, index) => (
-      <Box
-        key={index}
-        sx={{ display: 'flex', gap: '10px', justifyContent: 'center' }}
-      >
-        <Typography align="left" sx={{ flexGrow: 1 }}>
-          {task}
-        </Typography>
-        <Button variant="contained">完了</Button>
-      </Box>
-    ));
-  };
 
   return (
     <Container sx={{ display: 'grid', gap: '10px' }}>
       <Header />
       <AddTaskForm />
-      <Box>
-        <Box>
-          <Typography variant="h4">タスク一覧</Typography>
-        </Box>
-        <Box sx={{ display: 'grid', gap: '10px' }}>{renderTodoList()}</Box>
-      </Box>
+      <TaskList tasks={tasks} />
     </Container>
   );
 };
