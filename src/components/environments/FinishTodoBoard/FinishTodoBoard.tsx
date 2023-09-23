ac8cbe56e5a392a00e51bdb917cd837a82e4820f
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-23 14:38:20 +0900

    #11 完了したタスクを追加するためのページの基礎作成

---
+++ src/components/environments/FinishTodoBoard/FinishTodoBoard.tsx
@@ -0,0 +1,7 @@
+const FinishTodoBoard = (): JSX.Element => (
+  <>
+    <div>完了したタスクを表示するよ</div>
+  </>
+);
+
+export default FinishTodoBoard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import FinishTodoBoard from '../components/environments/FinishTodoBoard/FinishTodoBoard';
 import TodoBoard from '../components/environments/TodoBoard/TodoBoard';
 
 const IndexRoutes = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<TodoBoard />} />
+    <Route path="finish" element={<FinishTodoBoard />} />
   </Routes>
 );
 
