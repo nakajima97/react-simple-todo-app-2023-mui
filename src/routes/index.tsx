0a7d28748ae0b1ca6b1a0965fa70289e39a841a2
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-23 14:39:42 +0900

    #11 todoではなくtasksという単語を使うようにした

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import FinishTodoBoard from '../components/environments/FinishTodoBoard/FinishTodoBoard';
-import TodoBoard from '../components/environments/TodoBoard/TodoBoard';
+import FinishTasksBoard from '../components/environments/FinishTasksBoard/FinishTasksBoard';
+import TasksBoard from '../components/environments/TasksBoard/TasksBoard';
 
 const IndexRoutes = (): JSX.Element => (
   <Routes>
-    <Route path="/" element={<TodoBoard />} />
-    <Route path="finish" element={<FinishTodoBoard />} />
+    <Route path="/" element={<TasksBoard />} />
+    <Route path="finish" element={<FinishTasksBoard />} />
   </Routes>
 );
 
