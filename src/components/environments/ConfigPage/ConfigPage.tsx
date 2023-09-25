6db49e541d4ac39cd748049983ed8597d90f7ad6
nakajima97 <kituneudon97@gmail.com>
2023-09-25 22:09:06 +0900

    #17 完了済みタスクを削除するコードを作成

--- src/components/environments/ConfigPage/ConfigPage.tsx
+++ src/components/environments/ConfigPage/ConfigPage.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Container, Typography } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Container, Typography } from '@mui/material';
+import DeleteTaskButton from '../../organisms/DeleteTaskButton/DeleteTaskButton';
 import Header from '../../organisms/Header/Header';
 
 const ConfigPage = (): JSX.Element => {
@@ -8,9 +8,7 @@ const ConfigPage = (): JSX.Element => {
       <Header />
       <Box>
         <Typography variant="h3">データ削除</Typography>
-        <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
-          データ削除
-        </Button>
+        <DeleteTaskButton />
         <Typography>登録しているタスクの情報をすべて削除します。</Typography>
         <Typography>完了済みタスクも削除されます。</Typography>
       </Box>
