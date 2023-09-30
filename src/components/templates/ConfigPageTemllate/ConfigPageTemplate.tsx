fe75a01f1717280ea7b0ad71543b66997d5e7c44
Nakajima <kituneudon97@gmail.com>
2023-09-30 15:55:35 +0900

    #24 名称変更 (#26)

--- src/components/templates/ConfigPageTemllate/ConfigPageTemplate.tsx
+++ src/components/templates/ConfigPageTemllate/ConfigPageTemplate.tsx
@@ -1,17 +1,12 @@
-import { Box, Container, Typography } from '@mui/material';
-import DeleteTaskButton from '../../organisms/DeleteTaskButton/DeleteTaskButton';
+import { Container } from '@mui/material';
+import DeleteTask from '../../organisms/DeleteTask/DeleteTask';
 import Layout from '../../organisms/Layout/Layout';
 
 const ConfigPage = (): JSX.Element => {
   return (
     <Container>
       <Layout />
-      <Box>
-        <Typography variant="h3">データ削除</Typography>
-        <DeleteTaskButton />
-        <Typography>登録しているタスクの情報をすべて削除します。</Typography>
-        <Typography>完了済みタスクも削除されます。</Typography>
-      </Box>
+      <DeleteTask />
     </Container>
   );
 };
